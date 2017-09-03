c64e8ce028e642bd5a8d82e15275069f3cacc3cd
mayartmal <mayartmal@gmail.com>
2017-09-03 11:54:17 +0300

    files rename

---
+++ practice_1_functions.py
@@ -0,0 +1,12 @@
+def function_name(argument_1, argument_2):
+    # function body
+    return(argument_2 + argument_1)
+
+
+x = function_name(1, 13)
+y = function_name(x, 21)
+print(x)
+
+print(type(function_name))
+print(type(type(function_name)))
+print(id(function_name))
